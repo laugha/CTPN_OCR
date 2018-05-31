3f42b6dc3946eac6919086fe9f045c8e01a23571
lihow <2458858175@qq.com>
2018-05-31 20:09:27 +0800

    后端程序

---
+++ demo.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+import model
+from glob import glob
+import numpy as np
+from PIL import Image
+import time
+paths = glob('./test/*.*')
+
+if __name__ =='__main__':
+    im = Image.open(paths[1])
+    img = np.array(im.convert('RGB'))
+    t = time.time()
+    result,img,angle = model.model(img,model='keras')
+    print ("It takes time:{}s".format(time.time()-t))
+    print ("---------------------------------------")
+    for key in result:
+        print (result[key][1])
